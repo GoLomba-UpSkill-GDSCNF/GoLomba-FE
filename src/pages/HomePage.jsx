8e9eaed06152b20a8392029e6240135b4e5fcbf4
Krisna Novianto <131007576+Krisn4novianto@users.noreply.github.com>
2023-08-17 21:05:57 +0700

    Update HomePage.jsx
    di update ada halaman login

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,8 @@
 import {Container, Row, Col} from "react-bootstrap";
 import HeroImage from "../assets/img/utama.png" ;
 import AboutImage from "../assets/img/about.png" ;
+import LoginImage from "../assets/img/login.png" ;
+import { Link } from 'react-router-dom';
 
 
 
@@ -9,6 +11,7 @@ import{kelasTerbaru} from "../data/index";
 
 // Membuat fungsi navigate
 import {useNavigate} from "react-router-dom";
+import buatakun from "../components/buatakun";
 
 const HomePage = () => {
 
@@ -18,7 +21,7 @@ const HomePage = () => {
 
   return (
   <div className="homepage">
-    <header className="w-100 min-vh-100 d-flex align-items-center  pt-lg-5">
+    <header className="w-100 min-vh-100 d-flex align-items-center">
     <Container>
         <Row className="header-box d-flex align-items-center">
           <Col lg="6">
@@ -41,8 +44,8 @@ const HomePage = () => {
       <Container>
         <Row>
           <Col>
-            <h1 className="text-center fw-bold">Kompetisi Terbaru</h1><br></br>
-            <p className="text-center fw-bold">Cari pengalaman yang Anda minati!</p>
+            <h1 className="text-center fw-bold">Kelas Terbaru</h1>
+            <p className="text-center fw-bold">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
           </Col>
         </Row>
 
@@ -79,35 +82,85 @@ const HomePage = () => {
       </Container>
     </div>
 
-
     <div className="about" py-5>
-      <Container>
-        <Row className="about-box d-flex align-items-center">
-          <Col lg="6">
-          <img src={AboutImage} alt="about.png" />
-          </Col>
+    <Container>
+      <Row className="about-box d-flex align-items-center">
+        <Col lg="6">
+        <img src={AboutImage} alt="about.png" />
+        </Col>
 
-          <Col lg="6" style={{ padding: '50px' }}>
-            
-            <h1 className="mb-2"><span>Mengapa </span><br></br> harus berprestasi?</h1><br></br>
+        <Col lg="6" style={{ padding: '50px' }}>
+          
+          <h1 className="mb-2"><span>Mengapa </span><br></br> harus berprestasi?</h1><br></br>
+          <p>
+          Prestasi memiliki dampak signifikan dalam menggambarkan dedikasi dan kemampuan individu. Melalui usaha dan ketekunan, prestasi menciptakan bukti konkret tentang pencapaian seseorang dalam berbagai bidang, baik akademis maupun profesional. Prestasi tidak hanya membangkitkan rasa percaya diri, tetapi juga membuka pintu peluang dalam lingkungan yang kompetitif.
+          </p>
+          <p>
+          Namun, penting untuk diingat bahwa prestasi tidaklah satu-satunya penentu keberhasilan atau kebahagiaan. Hubungan sosial yang kuat, keseimbangan hidup, serta pencarian makna dalam aktivitas sehari-hari juga berperan krusial dalam membentuk kualitas hidup yang seimbang dan memuaskan. Prestasi dapat menjadi sumber inspirasi dan pendorong menuju tujuan, tetapi harus diimbangi dengan elemen-elemen lain yang juga penting bagi kesejahteraan secara menyeluruh. 
+          </p>
+    
+
+        </Col>
+      </Row>
+    </Container>
+  </div>
+
+  <div className="login py-5">
+  <login>
+    <Container>
+        <Row className="box d-flex align-items-center">
+        <Col  lg="6">
+        <img src={LoginImage} alt="login.png" />
+        </Col>
+        
+        <Col lg="6">
+          <h2 className="login text-center fw-bold">Login</h2>
+          <form className="login-container">
             <p>
-            Prestasi memiliki dampak signifikan dalam menggambarkan dedikasi dan kemampuan individu. Melalui usaha dan ketekunan, prestasi menciptakan bukti konkret tentang pencapaian seseorang dalam berbagai bidang, baik akademis maupun profesional. Prestasi tidak hanya membangkitkan rasa percaya diri, tetapi juga membuka pintu peluang dalam lingkungan yang kompetitif.
+                <input type="email" placeholder="Email"></input>
             </p>
             <p>
-            Namun, penting untuk diingat bahwa prestasi tidaklah satu-satunya penentu keberhasilan atau kebahagiaan. Hubungan sosial yang kuat, keseimbangan hidup, serta pencarian makna dalam aktivitas sehari-hari juga berperan krusial dalam membentuk kualitas hidup yang seimbang dan memuaskan. Prestasi dapat menjadi sumber inspirasi dan pendorong menuju tujuan, tetapi harus diimbangi dengan elemen-elemen lain yang juga penting bagi kesejahteraan secara menyeluruh. 
+                <input type="password" placeholder="Password" class="mb-5"></input>
             </p>
+
+            <div class="text-center custom-container">
+
+            <button className="btn btn-danger btn-lg rounded-3 mb-xs-0 mb-2 custom-button">
+            Masuk
+            </button>
+
+            </div>
       
 
-          </Col>
-          
-          
+            <p align="center">
+               <a href="" class="custom-link" >Lupa kata sandi</a>
+            </p>
 
-          
+          <div class="text-center custom-container">
 
-        </Row>
-      </Container>
+          <button className="btn btn-outline-danger btn-lg rounded-3 mb-xs-0 mb-3 custom-button1">
+          Sign In with Google
+          </button>
 
-    </div>
+          </div>
+
+          <p align="center">
+          <Link to="/buatakun" class="custom-link" > Belum punya akun? </Link>
+          </p>
+
+
+
+
+          </form>
+        </Col>
+        
+      </Row>
+    </Container>
+    </login>
+  </div>
+    
+
+
     
   </div>
   
